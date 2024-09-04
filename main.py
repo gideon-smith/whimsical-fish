9ab8309ce9703ebbe9dcff8b077f41f908c75fe9
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-04 14:51:44 -0400

    Update main.py
    window opening and game runing

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import pygame
+pygame.init()
+screen = pygame.display.set_mode(1280,720)
+clock = pygame.time.Clock()
+runing = true
+#put varibeils here
+
+
+while runing:
+  for evint in pygame.event.get()
+     if event.type == pygame.QUIT:
+        running = False
+  #game updates here
+  
+  
+  
+  screen.fill("black")
+  #render game here
+  
+  
+  
+  
+  pygame.display.flip()
+  clock.tick(60)
+pygame.quit()
