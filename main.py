813c353dba99523b0a583d5135f117430099611a
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-05 18:33:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ dt = 0
 map_image = "dock.png"
 player_pos = pygame.Vector2(250, 250)
 map = pygame.image.load(map_image).convert()
-submap = map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
+temporary_map = map 
+submap = temporary_map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
 while running:
   for event in pygame.event.get():
      if pygame.event == pygame.QUIT:
@@ -20,26 +21,29 @@ while running:
   keys = pygame.key.get_pressed()
   if keys[pygame.K_w]:
       player_pos.y -= 300 * dt
-      submap = map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
+      submap = temporary_map.subsurface(player_pos.x - 100,player_pos.y - 100 ,player_pos.x + 100,player_pos.y + 100)
+      temporary_map = map
   if keys[pygame.K_s]:
       player_pos.y += 300 * dt
-      submap = map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
+      submap = temporary_map.subsurface(player_pos.x - 100,player_pos.y - 100 ,player_pos.x + 250,player_pos.y + 100)
+      temporary_map = map
   if keys[pygame.K_a]:
       player_pos.x -= 300 * dt
-      submap = map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
+      submap = temporary_map.subsurface(player_pos.x - 100,player_pos.y - 100,player_pos.x + 100,player_pos.y + 100)
+      temporary_map = map
   if keys[pygame.K_d]:
       player_pos.x += 300 * dt
-      submap = map.subsurface(player_pos.x - 250,player_pos.y - 250 ,player_pos.x + 250,player_pos.y + 250)
-
+      submap = temporary_map.subsurface(player_pos.x - 100,player_pos.y - 100 ,player_pos.x + 100,player_pos.y + 100)
+      temporary_map = map
   
   screen.fill("black")
   #render game here
-  screen.blit(submap, (0, 0) )
+  screen.blit(map, (0, 0) )
   pygame.draw.circle(screen , "red" , (w/2 , h/2) ,20)
   
   
   
   pygame.display.flip()
-  clock.tick(60)
+  dt = clock.tick(60) / 1000
 pygame.quit()
 sys.exit()
