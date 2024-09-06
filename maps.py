3d9d4e06cd9fa7cbb3744131703e7c87e37c5339
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-06 19:00:48 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -6,9 +6,9 @@ class lake_water:
   type = 1
   file = "lake_tile"
 map = [1,2,3,4,5,6]
+map[0] = sand()
 map[1] = sand()
 map[2] = sand()
-map[3] = sand()
+map[3] = lake_water()
 map[4] = lake_water()
 map[5] = lake_water()
-map[6] = lake_water()
