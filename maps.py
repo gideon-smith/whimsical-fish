7c354a75d1d815181c805b1f96834c99fb7f7dc1
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-06 19:12:08 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,10 +1,10 @@
 class sand:
   type = 0
-  file = "sand_tile"
+  file = sand_tile
   sub_type = 0
 class lake_water:
   type = 1
-  file = "lake_tile"
+  file = lake_tile
 map = [1,2,3,4,5,6]
 map[0] = sand()
 map[1] = sand()
