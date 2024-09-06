1e6075ae43fcf8536dba7c871a77b84787443bba
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-06 19:53:35 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,7 +1,8 @@
+import main
 map = [1,2,3,4,5,6]
-map[0] = sand()
-map[1] = sand()
-map[2] = sand()
-map[3] = lake_water()
-map[4] = lake_water()
-map[5] = lake_water()
+map[0] = main.sand()
+map[1] = main.sand()
+map[2] = main.sand()
+map[3] = main.lake_water()
+map[4] = main.lake_water()
+map[5] = main.lake_water()
