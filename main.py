5999007fbead2e783616c52e7a84c399cd43605b
Logan Caramanna <106547747+bnobu@users.noreply.github.com>
2024-09-06 09:25:45 -0400

    Fix window not closing

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 import pygame
 import sys
+
 pygame.init()
-screen = pygame.display.set_mode()
+
+screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 running = True
-dt = 0
-#put varibeils here
+
 map_image = "dock.png"
 player_pos = pygame.Vector2(250, 250)
-map = pygame.image.load(map_image).convert()
-def render(image , x , y):
-    screen.blit(image, (x -player_pos.x,y - player_pos.y) )
+map_image_surface = pygame.image.load(map_image).convert()
+
+def render(image, x, y):
+    screen.blit(image, (x - player_pos.x, y - player_pos.y))
+
+movement_speed = 300
 
 while running:
-  for event in pygame.event.get():
-     if pygame.event == pygame.QUIT:
-        running = False
-        sys.exit()
-  #game updates here
-  w, h = pygame.display.get_surface().get_size()   
-  keys = pygame.key.get_pressed()
-  if keys[pygame.K_w]:
-      player_pos.y -= 300 * dt
-  if keys[pygame.K_s]:
-      player_pos.y += 300 * dt
-  if keys[pygame.K_a]:
-      player_pos.x -= 300 * dt
-  if keys[pygame.K_d]:
-      player_pos.x += 300 * dt
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-  
-  screen.fill("black")
-  #render game here
-  render(map ,0,0)
-  pygame.draw.circle(screen , "red" , (w/2 , h/2) ,20)
-  
-  
-  
-  pygame.display.flip()
-  dt = clock.tick(60) / 1000
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= movement_speed / 60
+    if keys[pygame.K_s]:
+        player_pos.y += movement_speed / 60
+    if keys[pygame.K_a]:
+        player_pos.x -= movement_speed / 60
+    if keys[pygame.K_d]:
+        player_pos.x += movement_speed / 60
+
+    screen.fill((0, 0, 0))
+    render(map_image_surface, 0, 0)
+    w, h = pygame.display.get_surface().get_size()
+    pygame.draw.circle(screen, (255, 0, 0), (w // 2, h // 2), 20)
+
+    pygame.display.flip()
+    clock.tick(60)
+
 pygame.quit()
 sys.exit()
+
