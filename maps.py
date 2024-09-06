396bcabff69bfd554df258c61494bcdacce21ef0
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-06 18:34:51 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,1 +1,14 @@
-
+class sand:
+  type = 0
+  file = "sand.png"
+  sub_type = 0
+class lake_water:
+  type = 1
+  file = "lake_water.png"
+map = []
+map[1] = sand()
+map[2] = sand()
+map[3] = sand()
+map[4] = lake_water()
+map[5] = lake_water()
+map[6] = lake_water()
