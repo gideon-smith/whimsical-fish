cbcfb944bce574df099c507056d12c6d7d9d5114
gideon smith <136742706+gideon-smith@users.noreply.github.com>
2024-09-06 19:09:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ while running:
     w, h = pygame.display.get_surface().get_size()
     for i in range(math.floor(len(map) / map_size)):
         for ii in range(map_size):
-            render(map[i].file)
+            render(map[i].file, ii , i)
     
         
     pygame.draw.circle(screen, (255, 0, 0), (w // 2, h // 2), 20)
